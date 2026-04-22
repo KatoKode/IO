bd51dd4e52d425fb92445af3421851379a773647
Data Structures and Algorithms in Assembly and C. <mcintosh1798@gmail.com>
2026-04-22 13:32:46 -0500

    Add files via upload

---
+++ io_make.sh
@@ -0,0 +1,42 @@
+#-------------------------------------------------------------------------------
+#   IO Implementation in x86_64 Assembly Language with C Interface
+#   Copyright (C) 2025  J. McIntosh
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#-------------------------------------------------------------------------------
+#
+clear;
+
+sep="--------------------------------------------------------------------------------"
+
+builtin cd ./io || exit -1
+
+make clean; make
+
+test -e ./libio.so || exit -1
+
+echo -e "${sep}"
+
+builtin cd ../demo || exit -1
+
+make clean; make
+
+test -e ./demo || exit -1
+
+chmod 744 ./go_demo.sh || exit -1
+
+echo -e "${sep}"
+
+echo -e "alls well that ends well.\n"
